3ce18cd26d775b20d298aa6f077468df4d790e5b
vinit <vinitsxn@gmail.com>
2021-12-25 23:24:32 +0530

    Added: Welcome message added

---
+++ src/snakeandladder/SnakeAndLadder.java
@@ -0,0 +1,11 @@
+//import package
+package snakeandladder;
+
+
+//class
+public class SnakeAndLadder {
+    // main method
+    public static void main(String[] args){
+        System.out.println("Welcome to Snake and Ladder Game"); //welcome message
+    }
+}
