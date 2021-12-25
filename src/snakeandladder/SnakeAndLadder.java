a8506e91ca1c7f4bbee0f775c98ddbe35f8ee284
vinit <vinitsxn@gmail.com>
2021-12-25 23:44:28 +0530

    Added: Rolling Dice functionality added

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -1,18 +1,28 @@
 //import package
 package snakeandladder;
-
+import java.util.Random;
 
 //class
 public class SnakeAndLadder {
+    //instance variable declaration
+    public static Random random=new Random();
+
     //method to display message
     public static void display(String s,String position){
         System.out.println(s+" "+position);
+    }
+    //method to roll dice
+    public static int rollDice(){
+        return (random.nextInt(6)+1);
     }
     // main method
     public static void main(String[] args){
         //variable declaration
         int currentPos=0;
+        int dice;
         System.out.println("Welcome to Snake and Ladder Game"); //welcome message
-        display("You are on: ",Integer.toString(0)); //start position
+        display("You are on: ",Integer.toString(currentPos)); //start position
+        dice=rollDice(); //roll a dice
+        display("You got: ", Integer.toString(dice)); //Display value of dice
     }
 }
