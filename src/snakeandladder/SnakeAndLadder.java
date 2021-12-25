68cce6387652cd8aaedf73a8680c198d2d5ba68e
vinit <vinitsxn@gmail.com>
2021-12-25 23:35:19 +0530

    Added: Start position added

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -4,8 +4,15 @@ package snakeandladder;
 
 //class
 public class SnakeAndLadder {
+    //method to display message
+    public static void display(String s,String position){
+        System.out.println(s+" "+position);
+    }
     // main method
     public static void main(String[] args){
+        //variable declaration
+        int currentPos=0;
         System.out.println("Welcome to Snake and Ladder Game"); //welcome message
+        display("You are on: ",Integer.toString(0)); //start position
     }
 }
